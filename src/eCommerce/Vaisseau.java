3017a6afb152e596dbd797eed5f7ce9b513c29a4
Kamil-univ <71633044+Kamil-univ@users.noreply.github.com>
2022-02-01 21:53:02 -0500

    first push

---
+++ src/eCommerce/Vaisseau.java
@@ -0,0 +1,13 @@
+package eCommerce;
+
+public class Vaisseau {
+	protected double capaciteMax;
+	protected double stockage;
+	
+	
+	public void charger() {
+		
+	}
+	public void decharger(){
+		}
+	}
